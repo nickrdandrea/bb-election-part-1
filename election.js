4bb12e11963c624cf8d7fa08503ac22e33ba25e2
nickrdandrea <nickrdandrea@gmail.com>
2017-02-21 12:13:41 -0500

    Add AJAX request that prints the JSON response in an alert.

--- election.js
+++ election.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
+  var httpRequest;
+  makeRequest('https://bb-election-api.herokuapp.com/');
 
-  // Imagination!
+  function makeRequest(url) {
+    httpRequest = new XMLHttpRequest();
 
-});
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = displayContents;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
+
+  function displayContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        var response = JSON.parse(httpRequest.responseText);
+        alert(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+
+  // function alertContents() {
+  //   if (httpRequest.readyState === XMLHttpRequest.DONE) {
+  //     if (httpRequest.status === 200) {
+  //       alert(httpRequest.responseText);
+  //     } else {
+  //       alert('There was a problem with the request.');
+  //     }
+  //   }
+  // }
+})();
